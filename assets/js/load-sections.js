ad5f3366dcec20edf1fc94f8c7106ac01ca5d8ab
Anthony Delgado <anthonydelgado@MacBook-Air-de-Anthony.local>
2025-04-20 12:53:13 -0400

    Test Bootsrap

--- assets/js/load-sections.js
+++ assets/js/load-sections.js
@@ -1,5 +1,4 @@
 // Carga futura de secciones, dashboards u optimización de navegación
 document.addEventListener('DOMContentLoaded', () => {
-  console.log("Portafolio cargado.");
-  // Puedes incluir lógica para cargar datos desde archivos externos más adelante
+  console.log("Portafolio BI & Cloud cargado correctamente.");
 });
