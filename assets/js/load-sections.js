db50f25473249fe1b3c0aa438c877bf20fa5c15c
Anthony Delgado <anthonydelgado@MacBook-Air-de-Anthony.local>
2025-04-20 12:44:19 -0400

    Test Bootsrap

--- assets/js/load-sections.js
+++ assets/js/load-sections.js
@@ -1,18 +1,14 @@
 const loadSection = (id, path) => {
   fetch(path)
     .then(res => res.text())
-    .then(data => document.getElementById(id).innerHTML = data);
+    .then(html => document.getElementById(id).innerHTML = html);
 };
 
 window.onload = () => {
-  loadSection("header", "/sections/header.html");
-  loadSection("nav", "/sections/nav.html");
-  loadSection("home", "/sections/inicio.html");
   loadSection("bi", "/sections/bi.html");
   loadSection("cloud", "/sections/cloud.html");
   loadSection("modelado", "/sections/modelado.html");
   loadSection("etl", "/sections/etl.html");
   loadSection("analisis", "/sections/analisis.html");
   loadSection("contacto", "/sections/contacto.html");
-  loadSection("footer", "/sections/footer.html");
 };
